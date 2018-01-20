e78a99e37355fa91d7f82811d9922229447e3180
Curtis Ostle <curtisostle@gmail.com>
2018-01-19 23:16:47 -0500

    logging weather data from api

--- app/components/Location.js
+++ app/components/Location.js
@@ -1,28 +1,51 @@
 const React = require('react');
+const api = require('../utils/api');
 
 class Location extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      zip:""
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    var value = event.target.value;
+
+    this.setState(function() {
+      return {
+        zip:value
+      }
+    })
+  }
+  handleClick(e) {
+    e.preventDefault();
+    api.getWeather(this.state.zip)
+  }
+
   render() {
     return (
       <div className='location-container'>
         <div className='location'>
           <label htmlFor='location' className='location-label'>
-            Enter a City and State
+            Enter a Zip Code
           </label>
           <input
             id='location'
-            placeholder='Rockville, Maryland'
+            placeholder='20817'
             type='text'
             autoComplete='off'
-            // value={this.state.username}
-            // onChange={this.handleChange}
+            value={this.state.zip}
+            onChange={this.handleChange}
             className="location-input"
           />
           <button
             className='button'
             type='submit'
-            // disabled={!this.state.username}>
-          >
-          Submit
+            disabled={!this.state.zip}
+            onClick={this.handleClick}>
+            Submit
           </button>
         </div>
       </div>
---
+++ app/utils/api.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+const key = 'b87ef13e00bc9136960cd7c0541a91b3'
+
+module.exports = {
+  getWeather: function(zip) {
+    let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${key}`
+    return axios.get(url).then((response) => {
+      console.log(response)
+    })
+  }
+}
